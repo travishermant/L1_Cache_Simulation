3ba88d1a57dba91c0f4b8aacbd1fef9c0511439b
alexpinz <43862713+alexpinz@users.noreply.github.com>
2018-11-20 17:15:46 -0800

    Update LRU.c

--- Source/C/LRU.c
+++ Source/C/LRU.c
@@ -5,4 +5,33 @@
 	LRU = 0, MRU = 7
 */
 
-#include "
+#include "main.h"
+extern struct cache thecache;
+
+int lru_update(int set ,int way, int tag, int hit, int miss, int lru)  //Use struct cache variables
+{
+	int check = 0;
+	
+	if((hit || miss) && thecache[set][way] != NULL){
+	// Set way with current tag to MRU
+	// Decrement LRU bits to the right of the MRU
+		thecache[set][way].lru = 7;
+		while(way != 8){
+			++way;
+			thecache[set][way].lru = (lru - 1);
+		}
+	check = 0;
+	}
+	elseif(thecache[set][way] == NULL){
+	// Initializing the cache, so NULL means empty way
+		thecache[set][way].lru = way - 1;
+	check = 0;
+	}
+	else
+		check = 1;
+	
+// Returns 0 if went through successfully
+// Else returns 1
+	
+reutrn check;
+}
