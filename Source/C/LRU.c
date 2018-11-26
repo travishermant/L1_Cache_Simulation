0bc03759e00e8499afce272bb533923eb53ffd7f
alexpinz <43862713+alexpinz@users.noreply.github.com>
2018-11-25 16:37:50 -0800

    Update LRU.c

--- Source/C/LRU.c
+++ Source/C/LRU.c
@@ -2,29 +2,30 @@
 	ECE 485
 	Travis Hermant, Alex Pinzon, Abdullah Barghouti, Ammar Khan
 
-	LRU = 0, MRU = 7
+	LRU = 0, MRU = 7 for DATA CACHE
+	LRU = 0, MRU = 3 for INSTRUCTION CACHE
 */
 
 #include "LRU.h"
 extern struct cache	Inst_Cache[SETS][INST_WAY], Data_Cache[SETS][DATA_WAY];
 
-int InstUpdateLRU(int set, int way, int lru)  //Use struct cache variables
+int InstUpdateLRU(int set, int way)  //Use struct cache variables
 {
 	int check = 0;
 	
-	if(Inst_Cache[set][way].lru >= 0 ){
+	if(Inst_Cache[SETS][INST_WAY].lru >= 0 ){
 	// Set way with current tag to MRU
 	// Decrement LRU bits to the right of the MRU
-		Inst_Cache[set][way].lru = 7;
-		while(way != 8){
+		Inst_Cache[SETS][INST_WAY].lru = 3;
+		while(way != 4){
 			++way;
-			Inst_Cache[set][way].lru = (lru - 1);
+			Inst_Cache[SETS][INST_WAY].lru = (Inst_Cache[SETS][INST_WAY].lru - 1);
 		}
 	check = 0;
 	}
-	else if(Inst_Cache[set][way].lru == -1){
-	// Initializing the cache, so NULL means empty way
-		Inst_Cache[set][way].lru = way - 1;
+	else if(Inst_Cache[SETS][INST_WAY].lru == -1){
+	// Initializing the cache, so -1 means empty way
+		Inst_Cache[SETS][INST_WAY].lru = way - 1;
 		check = 0;
 	}
 	else
@@ -36,23 +37,23 @@ int InstUpdateLRU(int set, int way, int lru)  //Use struct cache variables
 return check;
 }
 
-int DataUpdateLRU(int set, int way, int lru)  //Use struct cache variables
+int DataUpdateLRU(int set, int way)  //Use struct cache variables
 {
 	int check = 0;
 	
-	if(Data_Cache[set][way].lru >= 0){
+	if(Data_Cache[SETS][DATA_WAY].lru >= 0){
 	// Set way with current tag to MRU
 	// Decrement LRU bits to the right of the MRU
-		Data_Cache[set][way].lru = 7;
+		Data_Cache[SETS][DATA_WAY].lru = 7;
 		while(way != 8){
 			++way;
-			Data_Cache[set][way].lru = (lru - 1);
+			Data_Cache[SETS][DATA_WAY].lru = (Data_Cache[SETS][DATA_WAY].lru - 1);
 		}
 	check = 0;
 	}
-	else if(Data_Cache[set][way].lru == -1){
+	else if(Data_Cache[SETS][DATA_WAY].lru == -1){
 	// Initializing the cache, so NULL means empty way
-		Data_Cache[set][way].lru = way - 1;
+		Data_Cache[SETS][DATA_WAY].lru = way - 1;
 		check = 0;
 	}
 	else
