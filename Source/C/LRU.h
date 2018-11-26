76ded8d1439f8d248f2d6cd5d70e916833f229a5
alexpinz <43862713+alexpinz@users.noreply.github.com>
2018-11-25 18:13:17 -0800

    Update LRU.h

--- Source/C/LRU.h
+++ Source/C/LRU.h
@@ -7,6 +7,8 @@
 
 #include "main.h"
 
+extern struct cache	Inst_Cache[SETS][INST_WAY], Data_Cache[SETS][DATA_WAY];
+
 //Prototypes
 int InstUpdateLRU(int set ,int way);  //Use struct cache variables
 int DataUpdateLRU(int set ,int way);  //Use struct cache variables
