14ba8d9be726bc243cba0f10cf7dd11a29e91458
alexpinz <43862713+alexpinz@users.noreply.github.com>
2018-11-25 17:19:59 -0800

    Update MESI.c

--- Source/C/MESI.c
+++ Source/C/MESI.c
@@ -15,39 +15,43 @@ int UpdateMESI(int set, int way, int n /* 'n' from trace file */){
 
 	int check;
 	
-	if(thecache[set][way] == NULL){
-	//Empty way set MESI as invalid
-		thecache[set][way].mesi = 3;
-		check = 0;
-	}
-	else if((n == 0) && (thecache[set][way].mesi == 3)){
+	if((n == 0) && (Data_Cache[SETS][DATA_WAY].mesi == 3)){
 	//Moving from invalid to exclusive
-		thecache[set][way].mesi = 1;
+		Data_Cache[SETS][DATA_WAY].mesi = 1;
 		check = 0;
 	}
-	else if((n == 0) && (thecache[set][way].mesi == 1)){
+	else if((n == 0) && (Data_Cache[SETS][DATA_WAY].mesi == 1)){
 	//Moving from exclusive to shared, assuming other processor is reading
-		thecache[set][way].mesi = 2;
+		Data_Cache[SETS][DATA_WAY].mesi = 2;
 		check = 0;
 	}
-	else if((n == 0) && (thecache[set][way].mesi == 2)){
+	else if((n == 0) && (Data_Cache[SETS][DATA_WAY].mesi == 2)){
 	//If read command stay in shared state
-		thecache[set][way].mesi = 2;
+		Data_Cache[SETS][DATA_WAY].mesi = 2;
 		check = 0;
 	}
-	else if((n == 1) && (thecache[set][way].mesi == 2)){
+	else if((n == 1) && (Data_Cache[SETS][DATA_WAY].mesi == 2)){
 	//If write command and in shared state go to modified
-		thecache[set][way].mesi = 0;
+		Data_Cache[SETS][DATA_WAY].mesi = 0;
 		check = 0;
 	}
-	else if((n == 1) && (thecache[set][way].mesi == 1)){
+	else if((n == 1) && (Data_Cache[SETS][DATA_WAY].mesi == 1)){
 	//If write command and in the exclusive state go to modified
-		thecache[set][way].mesi = 0;
+		Data_Cache[SETS][DATA_WAY].mesi = 0;
 		check = 0;
 	}
-	else if((n == 0 || n == 1) && (thecache[set][way].mesi == 0)){
+	else if((n == 0 || n == 1) && (Data_Cache[SETS][DATA_WAY].mesi == 0)){
 	//Either read or write command, stay in modified state
-		thecache[set][way].mesi = 0;
+		Data_Cache[SETS][DATA_WAY].mesi = 0;
+		check = 0;
+	}
+	else if((n == 4)){
+	//Response to snooping, data request from L2
+		Inst_Cache[SETS][INST_WAY].mesi = 2;
+		check = 0;
+	}
+	else if(n == 3){
+		Inst_Cache[SETS][INST_WAY].mesi = 3;
 		check = 0;
 	}
 	else
