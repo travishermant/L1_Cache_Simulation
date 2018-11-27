eddda091cd7441e06c81f1b1d110e41a00b0a77b
travishermant <travishermant@gmail.com>
2018-11-26 19:30:26 -0800

    Fixed LRU

--- Source/C/LRU.c
+++ Source/C/LRU.c
@@ -12,19 +12,19 @@ int InstUpdateLRU(int set, int way)  //Use struct cache variables
 {
 	int check = 0;
 	
-	if(Inst_Cache[SETS][INST_WAY].lru >= 0 ){
+	if(Inst_Cache[set][way].lru >= 0 ){
 	// Set way with current tag to MRU
 	// Decrement LRU bits to the right of the MRU
-		Inst_Cache[SETS][INST_WAY].lru = 3;
+		Inst_Cache[set][way].lru = 3;
 		while(way != 4){
 			++way;
-			Inst_Cache[SETS][INST_WAY].lru = (Inst_Cache[SETS][INST_WAY].lru - 1);
+			Inst_Cache[set][way].lru = (Inst_Cache[set][way].lru - 1);
 		}
 	check = 0;
 	}
-	else if(Inst_Cache[SETS][INST_WAY].lru == -1){
+	else if(Inst_Cache[set][way].lru == -1){
 	// Initializing the cache, so -1 means empty way
-		Inst_Cache[SETS][INST_WAY].lru = way - 1;
+		Inst_Cache[set][way].lru = way - 1;
 		check = 0;
 	}
 	else
@@ -40,19 +40,19 @@ int DataUpdateLRU(int set, int way)  //Use struct cache variables
 {
 	int check = 0;
 	
-	if(Data_Cache[SETS][DATA_WAY].lru >= 0){
+	if(Data_Cache[set][way].lru >= 0){
 	// Set way with current tag to MRU
 	// Decrement LRU bits to the right of the MRU
-		Data_Cache[SETS][DATA_WAY].lru = 7;
+		Data_Cache[set][way].lru = 7;
 		while(way != 8){
 			++way;
-			Data_Cache[SETS][DATA_WAY].lru = (Data_Cache[SETS][DATA_WAY].lru - 1);
+			Data_Cache[set][way].lru = (Data_Cache[set][way].lru - 1);
 		}
 	check = 0;
 	}
-	else if(Data_Cache[SETS][DATA_WAY].lru == -1){
+	else if(Data_Cache[set][way].lru == -1){
 	// Initializing the cache, so NULL means empty way
-		Data_Cache[SETS][DATA_WAY].lru = way - 1;
+		Data_Cache[set][way].lru = way - 1;
 		check = 0;
 	}
 	else
