9021b2368307c82d7dc9743964968e4045f71ec4
travishermant <travishermant@gmail.com>
2018-11-28 15:05:59 -0800

    Prettier yet

--- Source/C/main.c
+++ Source/C/main.c
@@ -10,7 +10,7 @@
                     /            \
                    /              \
            Inst_Cache             Data_Cache
-           / 	|	 \           /    |     \
+           /    |    \           /    |     \
         MESI    |    LRU       MESI   |     LRU
                 | 					  |	
               Stats                 Stats
