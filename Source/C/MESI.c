d70a5ca171f02bf93db76e7f46f6fb411a840157
travishermant <travishermant@gmail.com>
2018-11-28 13:13:26 -0800

    Added comments to MESI

--- Source/C/MESI.c
+++ Source/C/MESI.c
@@ -7,8 +7,14 @@
 
 #include "MESI.h"
 
+/*
+L1_READ_DATA	n = 0	// read data request to L1 data cache
+L1_WRITE_DATA	n = 1	// write data request to L1 data cache
+L1_READ_INST	n = 2	// instruction fetch (a read request to L1 instruction cache)
+L2_INVALID		n = 3	// invalidate command from L2
+L2_SNOOP_DATA	n = 4	// data request from L2 (in response to snoop)
+*/
 int UpdateMESI(int set, int way, int n /* 'n' from trace file */){
-
 	int check;
 	
 	if((n == 0) && (Data_Cache[set][way].mesi == I)){
@@ -28,6 +34,11 @@ int UpdateMESI(int set, int way, int n /* 'n' from trace file */){
 		Data_Cache[set][way].mesi = S;
 		check = 0;
 		miss = FALSE;
+	}
+	else if((n == 0 || n == 1) && (Data_Cache[set][way].mesi == M)){
+	//Either read or write command, stay in modified state
+		Data_Cache[set][way].mesi = M;
+		check = 0;
 	}
 	if((n == 0) && (Data_Cache[set][way].mesi == S)){
 	//Moving from invalid to exclusive
@@ -49,23 +60,8 @@ int UpdateMESI(int set, int way, int n /* 'n' from trace file */){
 		Data_Cache[set][way].mesi = M;
 		check = 0;
 	}
-	else if((n == 0 || n == 1) && (Data_Cache[set][way].mesi == M)){
-	//Either read or write command, stay in modified state
-		Data_Cache[set][way].mesi = M;
-		check = 0;
-	}
-	else if(n == 4){
-	//Response to snooping, data request from L2
-		while(way < 8){
-			if(Data_Cache[set][way].tag == temp_tag){
-				if((Data_Cache[set][way].mesi == M) && (mode == 1))
-					printf("Return data to L2    <0x%lx>\n", (long)address);
-				Data_Cache[set][way].mesi = S;
-				DataUpdateLRU(set,way);
-				return 0;
-			}
-			way++;
-		}
+	else if(n == 2){
+		Inst_Cache[set][way].mesi =  S; //??
 		check = 0;
 	}
 	else if(n == 3){
@@ -80,8 +76,18 @@ int UpdateMESI(int set, int way, int n /* 'n' from trace file */){
 		}
 		check = 0;
 	}
-	else if(n == 2){
-		Inst_Cache[set][way].mesi =  S; //??
+	else if(n == 4){
+	//Response to snooping, data request from L2
+		while(way < 8){
+			if(Data_Cache[set][way].tag == temp_tag){
+				if((Data_Cache[set][way].mesi == M) && (mode == 1))
+					printf("Return data to L2    <0x%lx>\n", (long)address);
+				Data_Cache[set][way].mesi = S;
+				DataUpdateLRU(set,way);
+				return 0;
+			}
+			way++;
+		}
 		check = 0;
 	}
 	else
