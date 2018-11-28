438b1c4a02698d64e1732d3d6e3a85671ea56717
travishermant <travishermant@gmail.com>
2018-11-28 15:39:35 -0800

    fixing again

--- Source/C/data_cache.c
+++ Source/C/data_cache.c
@@ -67,12 +67,13 @@ void DataEvictLRU(int set_index, int new_tag){
 			if(mode == 1){
 				if((Data_Cache[set_index][i].mesi == M) && (n != 1){
 					printf("Write to L2 cache    <0x%lx>\n", (long)Data_Cache[set_index][i].address);
-				else if((Data_Cache[set_index][i].mesi == M) && (n == 1)
-					printf("Read for Ownership from L2    <0x%lx>\n", (long)address);
-				}
 				else{
 					printf("Read from L2    <0x%lx>\n", (long)address);
 				}
+				if(n == 1)
+					printf("Read for Ownership from L2    <0x%lx>\n", (long)address);
+				}
+				
 			}
 			UpdateMESI(set_index, i, n);
 			Data_Cache[set_index][i].address = address;
